81ce7e0f11cfbebede837e7b27c102b8497a75bf
Periyaun <periyaung123@gmail.com>
2025-12-30 17:08:30 -0500

    Baseline API reader

--- api/client.py
+++ api/client.py
@@ -1,17 +1,32 @@
 import requests
+import sys
 
-class JobAPIClient:
+class Job_Finder:
     def __init__(self, app_id, app_key):
         self.app_id = app_id
         self.app_key = app_key
-        self.base_url = "https://insert.com"
 
-    def fetch_jobs(self, query="python", location="us", limit=20):
+    def job_fetcher(self, location="us", query="any", limit="20"):
+        self.base_url = f"https://api.adzuna.com/v1/api/jobs/us/search/1?app_id={self.app_id}&app_key={self.app_key}&results_per_page={limit}&what={query}&content-type=application/json"
         params = {
-            "query": query,
             "location": location,
-            "limit": limit
-        }
-        response = requests.get(self.base_url, params=params)
-        response.raise_for_status()
-        return response.json()
+            "query": query,
+            "limit": limit,
+            "app_id": self.app_id,
+            "app_key": self.app_key
+
+        }     
+
+        response = requests.get(self.base_url)
+        data = response.json()
+
+        for job in data.get("results", []):
+            print(job.get("title"))
+            print(job.get("description"))
+            print("-" * 30)
+        
+
+test = Job_Finder("REDACTED_APP_ID", "REDACTED_APP_KEY")
+test.job_fetcher()
+
+       
